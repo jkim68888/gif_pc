81f46aa8bad01a50bad82b62b975cd810b8beda6
Jihyun <jkim68888@gmail.com>
2021-02-05 02:30:51 +0900

    모든 서브페이지 퍼블리싱 완료

--- js/join.js
+++ js/join.js
@@ -1,51 +1,103 @@
-//date
+//모두 체크 동의
 $(document).ready(function(){
-    var ySelect = $('#joinBox #birthYear');
-    var mSelect = $('#joinBox #birthMonth');
-    var dSelect = $('#joinBox #birthDate');
-    var today = new Date(); 
-    var y = today.getFullYear();
-    var yOutput = ''; 
+    $('#allChk').change(function(){
+        var chk = $(this).prop('checked');
+        if(chk){
+            $('.checkBox input').prop('checked',true);
+        }else{
+            $('.checkBox input').prop('checked',false);
+        }
+    });
+    $('.checkBox input').change(function(){
+        var chk01 = $('#agreeChk01').prop('checked');
+        var chk02 = $('#agreeChk02').prop('checked');
+
+        if(chk01 && chk02 ){
+            $('#allChk').prop('checked',true);
+        }else{
+            $('#allChk').prop('checked',false);
+        }
+    });
+    
+    $('.joinClause .nextBtn a').click(function(e){
+        e.preventDefault(); 
+        var chk01 = $('#agreeChk01').prop('checked');
+        var chk02 = $('#agreeChk02').prop('checked');
+        
+        if(chk01 && chk02 ){
+            location.assign('../tnav/joinForm.html#subTitle');
+        }else{
+            alert('모두 동의해주세요.');
+        }
+    });
+});
+
+
+//생년월일 처리
+$(document).ready(function(){
+    var yOutput = '';
     var mOutput = '';
     var dOutput = '';
     
-    for(var i=y;i>=1900;i--){
+    var today = new Date();
+    var tYear = today.getFullYear();
+    
+    for(var i=tYear;i>=1900;i--){
         yOutput += '<option>' + i + '</option>';
     }
-    
-    ySelect.html(yOutput);
-    
     for(var i=1;i<=12;i++){
         mOutput += '<option>' + i + '</option>';
     }
     
-    mSelect.html(mOutput);
+    $('#birth01').html(yOutput);
+    $('#birth02').html(mOutput);
     
-    var lastDate = [31,28,31,30,31,30,31,31,30,31,30,31];
-   
-    mSelect.change(function(){
+    function dateList(y,m){
         dOutput = ''; 
         
-        var m = $(this).val() - 1;
-        var y2 = ySelect.val();
-       
-        if((y2 % 4 == 0 && y2 % 100 != 0 ) || y2 % 400 == 0){
-            lastDate[1] = 29;   
-        }else{
-            lastDate[1] = 28; 
+        var mArray = [31,28,31,30,31,30,31,31,30,31,30,31];
+        
+        //윤년
+        if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0){
+            mArray[1] = 29;
         }
         
-        for(var i=1;i<=lastDate[m];i++){
+        for(var i=1;i<=mArray[m-1];i++){
             dOutput += '<option>' + i + '</option>';
         }
         
-        dSelect.html(dOutput);
+        $('#birth03').html(dOutput);
+    }
+    
+    dateList(tYear,1);
+    
+    $('#birth01, #birth02').change(function(){
+        var y = $('#birth01').val();
+        var m = $('#birth02').val();
+        
+        dateList(y,m);
+    });
+});
+
+
+
+
+//회원가입 전송 미처리 및 데이터 확인
+$(document).ready(function(){
+    $('#joinForm').submit(function(e){
+        e.preventDefault(); 
     });
     
-    ySelect.change(function(){
-        mSelect.trigger('change');
+    //비밀번호 확인
+    $('#joinPwChk').blur(function(){
+        var pw01 = $('#joinPw').val();
+        var pw02 = $(this).val();
+        
+        if(pw01 != pw02){
+            $('.joinClause .red').fadeIn(0);
+        }else{
+            $('.joinClause .red').fadeOut(0);
+        }
     });
-    
-    mSelect.trigger('change');
 });
 
